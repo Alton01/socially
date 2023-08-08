20214fc6623b01828d321938411e40e3a00e4bf6
Altono1 <altonobinna11@gmail.com>
2023-08-08 09:36:30 +0100

    version1

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,7 @@
+//this is imported from src/actions/posts.js
+//DONE TO PREVENT APP FROM  BREAKING
+
+export const CREATE = 'CREATE';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH_ALL';
